44f8a609b387de54e6606903f9bb88911a835f6b
ParkMinSung2580 <114042812+ParkMinSung2580@users.noreply.github.com>
2025-06-04 10:51:05 +0900

    [chore] TMP Essential, Harzard Prefab Setting
    -Unity UI TMP 리소스 추가
    -만들어 놓은 함정들 Prefab으로 보관
    -SavePoint 이미지 변경 및 다운받은 UIBundle Mutiple Sprite로 뭉쳐 있는거 개별 슬라이스(추후 UI로 사용 - 시간이 없어서 UI는 대충만들기로 함)
    -SavePoint 코루틴사용하여 플레이어나 플레이어 공격에 닿을시 0.5f동안 색상 변경 추가

--- Assets/Scripts/SaveController.cs
+++ Assets/Scripts/SaveController.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class SaveController : MonoBehaviour
 {
-     private void OnTriggerEnter2D(Collider2D collision)
+    private SpriteRenderer _sprite;
+
+    private void Start()
+    {
+        _sprite = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Bullet"))
         {
             Debug.Log(collision.name + "Save");
             GameManager.Instance._lastPlayerPosition = new Vector2(GameObject.Find("Player").transform.position.x, GameObject.Find("Player").transform.position.y);
+            collision.gameObject.SetActive(false);
+            StartCoroutine(ChangeColor());
             //GameManager.Instance._lastPlayerRb;
         }
         if (collision.CompareTag("Player"))
         {
             Debug.Log(collision.name + "Save");
             GameManager.Instance._lastPlayerPosition = new Vector2(collision.transform.position.x, collision.transform.position.y);
+            StartCoroutine(ChangeColor());
         }
+    }
+    
+    private IEnumerator ChangeColor()
+    {
+        _sprite.color = Color.red;
+        yield return new WaitForSeconds(0.5f);
+        _sprite.color = Color.white;
     }
 }
